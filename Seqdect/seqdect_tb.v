bdbaf607b28efb735916a20ddf0ea1784fda80c7
Surya-GS-28 <gssurya0228@gmail.com>
2025-12-27 16:37:04 +0530

    Updated logic

--- Seqdect/seqdect_tb.v
+++ Seqdect/seqdect_tb.v
@@ -6,37 +6,14 @@ seqdect DUT(.clk(clk),.rst(rst),.prtx(prtx),.prtz(prtz));
 initial begin
 clk=0;
 forever begin #5 clk=~clk; end 
-<<<<<<< HEAD
 end
-=======
 end
->>>>>>> 6470f23568a498135a895714c5244b89f265f72f
 //-----Apply RESET logic Here-----
 initial begin
   rst=1;
   #15 rst=0;
 end
 //-----TEST BENCH-----
-<<<<<<< HEAD
-initial begin
-//Variety of test cases.Remove comments to test one by one.
-
-/*//1st case
-#12 prtx=1;#10 prtx=0;#10 prtx=1;#10 prtx=0;#10 prtx=1;#10 prtx=0;
-//---End of 1st case---*/
-/*//2nd case
-#12 prtx=0;#10 prtx=1;#10 prtx=0;#10 prtx=1;#10 prtx=0;#10 prtx=1;#10 prtx=0;
-//---End of 2nd case---*/
-/*//3rd Case
-#12 prtx=0;#10prtx=1;#10 prtx=0;#10 prtx=1;#10 prtx=0;#10 prtx=1;#10 prtx=0;#10 prtx=1;#10 prtx=0;
-//---End of 3rd case---*/
-//4th case
-#12 prtx=0;#10 prtx=1;#10 prtx=0;#10 prtx=1;#10 prtx=0;#10 prtx=1;#10 prtx=0;#10 prtx=0;#10 prtx=1;#10 prtx=0;
-//---End of 4th case---
-/*5th case
-#12 prtx=0;#10 prtx=0; #10 prtx=1;#10 prtx=0;#10 prtx=1;#10 prtx=0;#10 prtx=1;#10 prtx=0;#10 prtx=1;#10 prtx=0;
-#10 prtx=0;#10 prtx=1;#10 prtx=0;#10 prtx=1;#10 prtx=0;#10 prtx=1;#10 prtx=0;
-=======
 initial begin
 //Variety of test cases.Remove comments to test one by one.
 
